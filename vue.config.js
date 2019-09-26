eb352ad1e4e0cd08f614297645fdd959b2b8982d
David Cumps <david@cumps.be>
2019-09-26 16:40:35 +0200

    fix: use public folder

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   publicPath: "/",
   outputDir: "site/dist",
   assetsDir: "static",
+  pages: {
+    index: {
+      entry: "./site/src/main.js",
+      template: "site/public/index.html",
+      filename: "index.html"
+    }
+  },
   chainWebpack: config => {
     config
       .entry("app")
@@ -11,6 +19,17 @@ module.exports = {
       .add("./site/src/main.js")
       .end();
     config.resolve.alias.set("@", path.join(__dirname, "./site/src"));
+  },
+  configureWebpack: config => {
+    config.plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: "site/public/",
+          to: ".",
+          ignore: ["index.html", ".DS_Store"]
+        }
+      ])
+    );
   },
   devServer: {
     contentBase: path.join(__dirname, "./site/public")
