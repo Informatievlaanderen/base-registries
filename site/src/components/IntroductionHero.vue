272b9aead54e694ecfe88d68acb096bac5b142ce
David Cumps <david@cumps.be>
2019-09-26 16:53:10 +0200

    feat: layout initial structure

---
+++ site/src/components/IntroductionHero.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    HERO HERE
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroductionHero"
+};
+</script>
--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="hero">
+    <IntroductionHero />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import IntroductionHero from "@/components/IntroductionHero.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    IntroductionHero
   }
 };
 </script>
