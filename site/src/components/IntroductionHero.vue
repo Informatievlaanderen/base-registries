c8e2ba20b84f0b897b2e62155a7b205916fee9fe
David Cumps <david@cumps.be>
2019-09-27 12:26:30 +0200

    feat: add images

--- site/src/components/IntroductionHero.vue
+++ site/src/components/IntroductionHero.vue
@@ -3,28 +3,28 @@
     <vl-hero-navigation-item
       title="Informatie"
       linkText="Verkennen"
-      background="#eee"
+      background="#eee url('img/home-VlaOverh.jpg')"
       to="/informatie">Bla bla bla bla</vl-hero-navigation-item>
 
     <vl-hero-navigation-item
       title="Documentatie"
       linkText="Bekijken"
       href="https://docs.basisregisters.vlaanderen"
-      background="#eee"
+      background="#eee url('img/home-VlaParl-2x.jpg')"
     >Bla</vl-hero-navigation-item>
 
     <vl-hero-navigation-item
       title="Registratie"
       href="https://www.vlaanderen.be/nl"
       linkText="Registreren"
-      background="#eee"
+      background="#eee url('img/home-VlaReg-2x.jpg')"
     >Bla</vl-hero-navigation-item>
 
     <vl-hero-navigation-item
       title="Ondersteuning"
       linkText="Vragen"
       href="https://www.vlaanderen.be/nl"
-      background="#eee"
+      background="#eee url('img/home-Gemeenten-2x.jpg')"
     >Bla</vl-hero-navigation-item>
   </vl-hero-navigation>
 </template>
