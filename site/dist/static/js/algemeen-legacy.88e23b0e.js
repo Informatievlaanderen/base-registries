48b84d9bd5ede118a037a9c8066a110697c36902
David Cumps <david@cumps.be>
2019-09-30 16:27:15 +0200

    chore(release): generate site [skip ci]

--- site/dist/static/js/algemeen-legacy.88e23b0e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["algemeen"],{"7fe8":function(e,n,l){"use strict";l.r(n);var t=function(){var e=this,n=e.$createElement,l=e._self._c||n;return l("vl-page",[l("vl-main")],1)},a=[],u=l("2877"),c={},r=Object(u["a"])(c,t,a,!1,null,null,null);n["default"]=r.exports}}]);
-//# sourceMappingURL=algemeen-legacy.88e23b0e.js.map
