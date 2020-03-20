966161e43a8916e8bd643ff07a34b433d4403715
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-03-20 16:30:27 +0100

    fix: inject alias_zone_name as env var

--- site/init.sh
+++ site/init.sh
@@ -3,4 +3,6 @@
 echo "window.baseRegistriesVersion=\"$API_VERSION\";" >> /usr/share/nginx/html/config.js
 echo "window.baseRegistriesApi=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.js
 
+sed -i "s/REPLACE_ALIAS_ZONE_NAME/$ALIAS_ZONE_NAME/g" /etc/nginx/conf.d/basisregisters.vlaanderen.conf
+
 nginx -g 'daemon off; load_module /etc/nginx/modules/ngx_http_perl_module-debug.so;'
