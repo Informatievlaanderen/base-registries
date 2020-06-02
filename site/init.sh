6a590c1c16b2e0e5cfc9be3d32904d321761d9c4
David Cumps <david@cumps.be>
2020-06-02 19:14:18 +0200

    fix: pass api version and site version to window

--- site/init.sh
+++ site/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "window.baseRegistriesVersion=\"$API_VERSION\";" >> /usr/share/nginx/html/config.js
+echo "window.baseRegistriesVersion=\"$SITE_VERSION\";" >> /usr/share/nginx/html/config.js
+echo "window.baseRegistriesApiVersion=\"$API_VERSION\";" >> /usr/share/nginx/html/config.js
 echo "window.baseRegistriesApi=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.js
 
 sed -i "s/REPLACE_ALIAS_ZONE_NAME/$ALIAS_ZONE_NAME/g" /etc/nginx/conf.d/basisregisters.vlaanderen.conf
