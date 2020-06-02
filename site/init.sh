9d61448d5ac3170b32fdbf444325a5eb9bc5f547
David Cumps <david@cumps.be>
2020-06-02 19:09:20 +0200

    feat: add VERSION to root

--- site/init.sh
+++ site/init.sh
@@ -5,4 +5,6 @@ echo "window.baseRegistriesApi=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/conf
 
 sed -i "s/REPLACE_ALIAS_ZONE_NAME/$ALIAS_ZONE_NAME/g" /etc/nginx/conf.d/basisregisters.vlaanderen.conf
 
+echo $SITE_VERSION > /usr/share/nginx/html/VERSION
+
 nginx -g 'daemon off; load_module /etc/nginx/modules/ngx_http_perl_module-debug.so;'
