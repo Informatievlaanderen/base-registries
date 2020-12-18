9c6e3ec6846d4104a0586759119d3f301d94a34c
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-12-18 18:40:00 +0100

    chore: add inial status placeholders

---
+++ site/src/views/Status/ImportStatus.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="todo-status-dashboard">
+    <h3 itemprop="name">Import - placeholder</h3>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+export default {
+  name: 'ImportStatus',
+  components: [],
+  props: {
+  },
+};
+</script>
+
---
+++ site/src/views/Status/RegistryStatus.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h3 itemprop="name" class="vl-registry-status-item__title">{{ title }}</h3>
+    <projection-status>{{ status.projections }}</projection-status>
+    <cache-status>{{ status.cache }}</cache-status>
+    <import-status>{{ status.import }}</import-status>
+  </div>
+</template>
+
+<style lang="scss">
+  .vl-registry-status-item__title {
+    color: #05c;
+    font-weight: 500;
+  }
+
+  .todo-status-dashboard {
+    border: 1px solid red;
+
+    :before {
+      color: red;
+      content: 'Todo:';
+      text-transform: uppercase;
+      padding: 5px;
+      font-weight: 600;
+    }
+  }
+</style>
+
+<script>
+import ProjectionStatus from './ProjectionStatus.vue';
+import CacheStatus from './CacheStatus.vue';
+import ImportStatus from './ImportStatus.vue';
+
+export default {
+  name: 'RegistryStatus',
+  components: { 
+    ProjectionStatus, 
+    CacheStatus, 
+    ImportStatus,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
