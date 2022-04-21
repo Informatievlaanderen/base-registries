fdb924ae2b290862b44bc149e471b7cd6bc98241
AnaisDC <49196256+AnaisDC@users.noreply.github.com>
2022-04-21 13:26:28 +0200

    Update IntroductionHero.vue
    Delete 2 navigation items

--- site/src/views/Home/IntroductionHero.vue
+++ site/src/views/Home/IntroductionHero.vue
@@ -16,22 +16,6 @@
       Hoe gebruik ik ze in mijn systemen? Leer het hier.
     </vl-hero-navigation-item>
 
-    <vl-hero-navigation-item
-      title="Registratie"
-      href="/portal"
-      link-text="Registreren"
-      background="#eee url('img/vlaamse-regering.jpg')">
-      Wie kan ze gebruiken? Hoe krijg ik toegang? Lees snel verder!
-    </vl-hero-navigation-item>
-
-    <vl-hero-navigation-item
-      title="Ondersteuning"
-      link-text="Vragen"
-      href="https://www.vlaanderen.be/nl"
-      background="#eee url('img/gemeenten.jpg')">
-      &#8220;Help! I need somebody&hellip;&#8221;<br>Krijg hier antwoord op al uw vragen&hellip;
-    </vl-hero-navigation-item>
-  </vl-hero-navigation>
 </template>
 
 <script>
