e94bc6e834fb2776b2a0a7c4cf25f6c4d589a30e
AnaisDC <49196256+AnaisDC@users.noreply.github.com>
2022-04-21 11:59:22 +0200

    Update ImplementationModel.vue
    Change implementatiemodel page

--- site/src/views/ImplementationModel.vue
+++ site/src/views/ImplementationModel.vue
@@ -10,19 +10,8 @@
           <div class="wp-pt-heading__title-wrapper">
             <vl-title tag-name="h1" class="wp-pt-heading__title">Implementatiemodel</vl-title>
           </div>
-
-          <vl-grid>
-            <vl-column width="9" width-s="12">
-              <div class="wp-pt-heading__content">
-                <vl-typography>
-                  <vl-introduction>
-                    Hoe zit het stelsel <strong>semantisch</strong> in elkaar? Bekijk de implementatiemodellen!
-                  </vl-introduction>
-                </vl-typography>
-              </div>
-            </vl-column>
-          </vl-grid>
         </header>
+        
       </vl-layout>
     </vl-region>
 
@@ -33,37 +22,41 @@
             <vl-column width="8" width-m="9" width-s="12">
               <vl-grid mod-stacked>
                 <vl-column width="12">
-                  <vl-title id="waarom" tag-name="h2">Waarom maken we dit?</vl-title>
-
-                  <vl-typography>
-                    <p>
-                      Semantische afstemming zorgt er voor dat informatiemodellen gebruik maken van eenduidige termen waar een bijbehorende definitie kan voor opgezocht worden. 
-                      Dit komt interoperabiliteit tussen toepassingen ten goede door dubbelzinnigheid en onduidelijkheid te elimineren. 
-                      Daarnaast zorgt het voor breder gedragen informatiemodellen door silodenken te vermijden.
-                    </p>
-                  </vl-typography>
-                </vl-column>
-
-                <vl-column width="12">
-                  <vl-title id="wat" tag-name="h2">Wat is een implementatiemodel precies?</vl-title>
                   <vl-typography>
                     <p>
                       Een implementatiemodel is een specificatie voor gegevensuitwisseling voor applicaties die een bepaalde use case vervullen. 
-                      Het laat toe om naast een gedeelde semantiek ook bijkomende restricties op te leggen,
-                      zoals het vastleggen van kardinaliteiten of het gebruik van bepaalde codelijsten. 
-                      Een implementatiemodel kan dienen als documentatie voor analisten en ontwikkelaars.
+                      Het laat toe om naast een gedeelde semantiek ook bijkomende restricties op te leggen, zoals het vastleggen van kardinaliteiten 
+                      of het gebruik van bepaalde codelijsten. 
+                      Een implementatiemodel dient als documentatie voor analisten, ontwikkelaars en dataleveranciers. 
+                    </p>
+                    <p>
+                    Het implementatiemodel bouwt voort op een reeks bestaande standaarden waaronder:
+                    <ul>
+                    <li>
+                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-adresregister.html">
+                    Applicatieprofiel adressenregister
+                    </a>
+                    </li>
+                     <li>
+                    <a href="https://data.vlaanderen.be/standaarden/erkende-standaard/vocabularium-adres.html">
+                    Vocabularium adressenregister
+                    </a>
+                    </li>
+                     <li>
+                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-gebouwenregister.html">
+                    Applicatieprofiel gebouwenregister
+                    </a>
+                    </li>
+                     <li>
+                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/vocabularium-gebouw.html">
+                    Vocabilarium gebouwenregister
+                    </a>
+                    </li>
+                    </ul>
                     </p>
                   </vl-typography>
-                </vl-column>
-
-                <vl-column width="12">
-                  <vl-title id="aanbod" tag-name="h2">Aanbod van implementatiemodellen</vl-title>
-
-                  <vl-typography class="vl-u-spacer--medium ">
-                    <p>Volgende implementatiemodellen maken momenteel deel uit van het stelsel:</p>
-                  </vl-typography>
-
-                  <vl-grid mod-stacked>
+                  
+                    <vl-grid mod-stacked>
                     <vl-column width="6" width-s="12">
                       <vl-doormat
                         title="Adressenregister"
@@ -80,24 +73,8 @@
                   </vl-grid>
                 </vl-column>
               </vl-grid>
-            </vl-column>
-
-            <vl-column
-              width="3"
-              push="1"
-              push-m="reset"
-              width-s="12"
-              class="wp-pt-side-navigation wp-pt-side-navigation--sticky-parent">
-              <aside class="wp-pt-side-navigation--sticky" style="top: 120px;">
-                <vl-side-navigation title="Op deze pagina">
-                  <vl-side-navigation-list>
-                    <vl-side-navigation-item href="#waarom" text="Waarom" />
-                    <vl-side-navigation-item href="#wat" text="Wat" />
-                    <vl-side-navigation-item href="#aanbod" text="Aanbod" />
-                  </vl-side-navigation-list>
-                </vl-side-navigation>
-              </aside>
-            </vl-column>
+                </vl-column>
+                
           </vl-grid>
         </vl-layout>
       </vl-region>
