3c5df749bd4194b06e77be9dec85e117cd418a7f
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-21 12:19:16 +0200

    fix: style formatting + build changes

--- site/src/views/ImplementationModel.vue
+++ site/src/views/ImplementationModel.vue
@@ -11,7 +11,7 @@
             <vl-title tag-name="h1" class="wp-pt-heading__title">Implementatiemodel</vl-title>
           </div>
         </header>
-        
+
       </vl-layout>
     </vl-region>
 
@@ -24,38 +24,38 @@
                 <vl-column width="12">
                   <vl-typography>
                     <p>
-                      Een implementatiemodel is een specificatie voor gegevensuitwisseling voor applicaties die een bepaalde use case vervullen. 
-                      Het laat toe om naast een gedeelde semantiek ook bijkomende restricties op te leggen, zoals het vastleggen van kardinaliteiten 
-                      of het gebruik van bepaalde codelijsten. 
-                      Een implementatiemodel dient als documentatie voor analisten, ontwikkelaars en dataleveranciers. 
+                      Een implementatiemodel is een specificatie voor gegevensuitwisseling voor applicaties die een bepaalde use case vervullen.
+                      Het laat toe om naast een gedeelde semantiek ook bijkomende restricties op te leggen, zoals het vastleggen van kardinaliteiten
+                      of het gebruik van bepaalde codelijsten.
+                      Een implementatiemodel dient als documentatie voor analisten, ontwikkelaars en dataleveranciers.
                     </p>
                     <p>
                     Het implementatiemodel bouwt voort op een reeks bestaande standaarden waaronder:
-                    <ul>
-                    <li>
-                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-adresregister.html">
-                    Applicatieprofiel adressenregister
-                    </a>
-                    </li>
-                     <li>
-                    <a href="https://data.vlaanderen.be/standaarden/erkende-standaard/vocabularium-adres.html">
-                    Vocabularium adressenregister
-                    </a>
-                    </li>
-                     <li>
-                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-gebouwenregister.html">
-                    Applicatieprofiel gebouwenregister
-                    </a>
-                    </li>
-                     <li>
-                    <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/vocabularium-gebouw.html">
-                    Vocabilarium gebouwenregister
-                    </a>
-                    </li>
-                    </ul>
+                      <ul>
+                        <li>
+                          <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-adresregister.html">
+                            Applicatieprofiel adressenregister
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://data.vlaanderen.be/standaarden/erkende-standaard/vocabularium-adres.html">
+                            Vocabularium adressenregister
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/applicatieprofiel-gebouwenregister.html">
+                            Applicatieprofiel gebouwenregister
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://data.vlaanderen.be/standaarden/kandidaat-standaard/vocabularium-gebouw.html">
+                            Vocabilarium gebouwenregister
+                          </a>
+                        </li>
+                      </ul>
                     </p>
                   </vl-typography>
-                  
+
                     <vl-grid mod-stacked>
                     <vl-column width="6" width-s="12">
                       <vl-doormat
@@ -73,8 +73,8 @@
                   </vl-grid>
                 </vl-column>
               </vl-grid>
-                </vl-column>
-                
+            </vl-column>
+
           </vl-grid>
         </vl-layout>
       </vl-region>
