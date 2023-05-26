67cf0d1b47777c13ac4a51df7f8dc16248c0d055
pgallik <patrickgallik@gmail.com>
2023-05-26 13:36:34 +0200

    feat: add faq & glossary modules + routes

---
+++ site/src/modules/glossary/index.ts
@@ -0,0 +1,11 @@
+import Root from "./views/Root.vue";
+
+export const GlossaryRoutes = [
+    {
+        path: "/verklarende-woordenlijst",
+        component: Root,
+        meta: {}
+    },
+];
+
+export default GlossaryRoutes;
