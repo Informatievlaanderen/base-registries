654d9169630e82c43713f8afa36a98d9703e7c81
pgallik <patrickgallik@gmail.com>
2023-06-02 11:25:35 +0200

    feat: add functional grar & wr views to release-notes

--- site/src/modules/release-notes/index.ts
+++ site/src/modules/release-notes/index.ts
@@ -1,5 +1,7 @@
 import Root from "./views/Root.vue";
 import ReleaseNotes from "./views/ReleaseNotes.vue";
+import FunctionalGrar from "./views/FunctionalGrar.vue";
+import FunctionalWr from "./views/FunctionalWr.vue";
 
 export const ReleaseNotesRoutes = [
     {
@@ -11,6 +13,16 @@ export const ReleaseNotesRoutes = [
                 path: "",
                 component: ReleaseNotes,
                 name: "release-notes"
+              },
+              {
+                path: "functioneelgrar",
+                component: FunctionalGrar,
+                name: "functionalgrar"
+              },
+              {
+                path: "functioneelwr",
+                component: FunctionalWr,
+                name: "functionalwr"
               },
         ],
     },
