65a12781860c7efec2c76437acd8f8b3f045adb1
pgallik <patrickgallik@gmail.com>
2023-06-08 12:02:20 +0200

    fix: add releasenotes backbutton in technicalgrar

--- site/src/modules/release-notes/views/ReleaseNotesTechnicalGrar.vue
+++ site/src/modules/release-notes/views/ReleaseNotesTechnicalGrar.vue
@@ -5,7 +5,7 @@
         <vl-layout>
           <vl-grid mod-stacked>
             <vl-column width="9" width-s="12">
-              <vl-link :to="$l('backButtons.home.url')" mod-bold v-l="`backButtons.home.text`" />
+              <vl-link :to="$l('backButtons.releaseNotes.url')" mod-bold v-l="`backButtons.releaseNotes.text`" />
               <header>
                 <y-markdown v-if="$data.markdownLoaded" :src="$data.header" />
               </header>
